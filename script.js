9d8dc5475aa5e12eefdbf6cade8d531fbf3ab376
Asteroid <aadrijamaitydps@gmail.com>
2023-09-18 18:23:05 +0530

    javascript and made yellow more darker for text

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"]
-console.log(colors)
+const btn = document.querySelector("#btn")
+const h2 = document.querySelector("h2")
+
+
+console.log(h2)
+btn.addEventListener("click", randomize)
+function randomize() {
+  let rn = Math.floor(Math.random() * colors.length)
+  let rFromArray = colors[rn]
+  h2.className = rFromArray
+  h2.innerHTML = rFromArray.replace(/^./, rFromArray[0].toUpperCase())
+}
